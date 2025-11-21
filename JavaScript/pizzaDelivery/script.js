b1a4190f191c3183cddc62ee71fb4ea7e6f6043f
nitish1445 <sarainitish@gmail.com>
2025-11-22 00:11:11 +0530

    pizza order

--- JavaScript/pizzaDelivery/script.js
+++ JavaScript/pizzaDelivery/script.js
@@ -8,21 +8,33 @@ function submit() {
   const cn = document.getElementById("cardNumber").value;
   const pn = document.getElementById("pinNumber").value;
   const an = document.getElementById("additionalNote").value;
-  const size = document.getElementById("size").value;
-  const size1 = document.getElementById("size1").value;
-  const size2 = document.getElementById("size2").value;
+  const sz = [];
+   document
+    .querySelectorAll("input[name='size']:checked")
+    .forEach((element) => {
+      sz.push(element.value);
+    });
+  const fv = [];
+   document
+    .querySelectorAll("input[name='flavour']:checked")
+    .forEach((element) => {
+      fv.push(element.value);
+    });  
+
+    alert("Order Confirmed Successfully!!")
 
   console.log("Full Name : " + fn);
   console.log("Last Name : " + ln);
   console.log("Email : " + em);
   console.log("Phone : " + ph);
   console.log("Date : " + dt);
+  console.log("Size : " + sz);
+  console.log("Flavour : " + fv);
   console.log("Card Number : " + cn);
   console.log("Pin Number : " + pn);
   console.log("Additional Notes : " + an);
   console.log("Size :" + size);
-  console.log("Size :" + size1);
-  console.log("Size :" + size2);
+  
 
   document.getElementById("firstName").value="";
   document.getElementById("lastName").value="";
@@ -32,7 +44,6 @@ function submit() {
   document.getElementById("cardNumber").value="";
   document.getElementById("pinNumber").value="";
   document.getElementById("additionalNote").value="";
-  document.getElementById("size").value="";
-  document.getElementById("size1").value="";
-  document.getElementById("size2").value="";
+  document.getElementsByName("flavour").value="";
+  document.getElementsByName("size").value="";
 }
