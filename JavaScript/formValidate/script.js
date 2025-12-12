c7a94814a7957067ff0774882389288b0f5b9d86
nitish1445 <sarainitish@gmail.com>
2025-12-12 23:40:27 +0530

    formValidate complete

--- JavaScript/formValidate/script.js
+++ JavaScript/formValidate/script.js
@@ -1,35 +1,59 @@
-function submit() {
-  const nm = document.getElementById("fullName").value;
-  const em = document.getElementById("email").value;
-  const mb = document.getElementById("mobileNo").value;
-  const dob = document.getElementById("dob").value;
+function Submit() {
+  const nm = document.getElementById("fullname").value.trim();
+  const em = document.getElementById("email").value.trim();
+  const ph = document.getElementById("phone").value.trim();
+  const db = document.getElementById("dob").value.trim();
 
-  // validation name
-  if (!/^[A-Za-z ]+$/.test(nm)) {
-    alert("Invalid Name");
+  document.querySelectorAll(".Error").forEach((element) => {
+    element.innerHTML = "";
+  });
+
+  //Validation Starts
+  if (!nm) {
+    document.getElementById("NameError").innerText = "Required";
+    return;
+  } else if (!/^[A-Za-z ]+$/.test(nm)) {
+    document.getElementById("NameError").innerText =
+      "Only Alphabets and Spaces are Allowed";
     return;
   }
-  // validation email
-  if (!/^[\w\.]+@(gmail|outlook|ricr)\.(com|in|co.in)$/.test(em)) {
-    alert("Invalid Email");
+
+  if (!em) {
+    document.getElementById("EmailError").innerText = "Required";
+    return;
+  } else if (!/^[\w\.]+@(gmail|outlook|ricr|yahoo)\.(com|in|co.in)$/.test(em)) {
+    document.getElementById("EmailError").innerText = "Use Proper Email Format";
     return;
   }
-//  mobile number validation
-  if(!/^[6-9]\d{9}$/.test(mb)){
-    alert("Invalid Mobile Number");
+
+  if (!ph) {
+    document.getElementById("PhoneError").innerText = "Required";
     return;
+  } else if (!/^[6-9]\d{9}$/.test(ph)) {
+    document.getElementById("PhoneError").innerText =
+      "Only Indian Mobile Nummber allowed";
+    return;
+  }
+
+  if (!db) {
+    document.getElementById("NameError").innerText = "Required";
+    return;
+  } else {
+    const currentyear = new Date().getFullYear();
+    const birthyear = Number(db.split("-")[0]);
+    if (currentyear - birthyear < 17) {
+      document.getElementById("DOBError").innerText =
+        "You must be 18 years Old";
+      return;
+    }
   }
 
   const data = {
     FullName: nm,
     Email: em,
-    Phone: mb,
-    DOB: dob,
+    Phone: ph,
+    DOB: db,
   };
-  console.log(data);
 
-  document.getElementById("fullName").value = "";
-  document.getElementById("email").value = "";
-  document.getElementById("mobileNo").value = "";
-  document.getElementById("dob").value = "";
+  console.log(data);
 }
