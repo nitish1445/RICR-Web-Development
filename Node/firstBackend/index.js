ecaddc5baed8553d92fa5e878286b103ef872f15
nitish1445 <sarainitish@gmail.com>
2026-01-11 15:50:51 +0530

    bakend classwork

--- Node/firstBackend/index.js
+++ Node/firstBackend/index.js
@@ -2,16 +2,23 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import connectDB from "./src/config/db.js";
-import AuthRouter from "./src/routers/myRouter.js"
+import AuthRouter from "./src/routers/myRouter.js";
 
 const app = express();
 
 app.use(express.json());
-app.use("/auth",AuthRouter);
+app.use("/auth", AuthRouter);
 
 app.get("/", (req, res) => {
   console.log("Server is loading......");
   res.json({ message: "Serever is Running Successfully" });
+});
+
+app.use((err, req, res, next) => {
+  const errorMessage = err.message || "Internal Server Error";
+  const StatusCode = err.statusCode;
+
+  res.status(StatusCode).json({ message: errorMessage });
 });
 
 const port = process.env.PORT || 5000;
--- Node/firstBackend/src/controllers/myController.js
+++ Node/firstBackend/src/controllers/myController.js
@@ -1,13 +1,20 @@
 import User from "../models/userModel.js";
 
-export const UserRegister = async (req, res) => {
+export const UserRegister = async (req, res, next) => {
   try {
     const { fullName, email, phone, password } = req.body;
     if (!fullName || !email || !phone || !password) {
-      res.status(400).json({ message: "All Fields Required" });
-      return;
+      const error = new Error("All Fields Required");
+      error.statusCode = 400;
+      return next(error);
     }
 
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      const error = new Error("Email Already Exists");
+      error.statusCode = 409;
+      return next(error);
+    }
     const newUser = await User.create({
       fullName,
       email,
@@ -20,28 +27,31 @@ export const UserRegister = async (req, res) => {
     res.status(201).json({ message: "User Created Successfully" });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Internal Server Error" });
+    next(error);
   }
 };
 
-export const UserLogin = async (req, res) => {
+export const UserLogin = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     if (!email || !password) {
-      res.status(400).json({ message: "All Fields Required" });
-      return;
+      const error = new Error("All Fields Required");
+      error.statusCode = 400;
+      return next(error);
     }
 
-    const existingUser = await User.find({ email });
+    const existingUser = await User.findOne({ email });
     if (!existingUser) {
-      res.status(404).json({ message: "User Not Found" });
-      return;
+      const error = new Error("User Not Found");
+      error.statusCode = 404;
+      return next(error);
     }
 
-    // const isVerified = password === existingUser.password;
-    if (password === existingUser.password) {
-      res.status(402).json({ message: "User Not Authorized" });
-      return;
+    const isVerified = password === existingUser.password;
+    if (!isVerified) {
+      const error = new Error("User Not Authorized");
+      error.statusCode = 402;
+      return next(error);
     }
 
     console.log(existingUser);
@@ -49,15 +59,45 @@ export const UserLogin = async (req, res) => {
     res.status(200).json({ message: "Welcome Back", data: existingUser });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Internal Server Error" });
+    next(error);
   }
 };
 
-export const UserLogout = async (req, res) => {
+export const UserLogout = async (req, res, next) => {
   try {
     res.status(200).json({ message: "Logout Successfull" });
   } catch (error) {
     console.log(error);
-    res.status(500).json({ message: "Internal Server Error" });
+    next(error);
+  }
+};
+
+export const UserUpdate = async (req, res, next) => {
+  try {
+    const { fullName, phone } = req.body;
+    if (!fullName|| !email || !phone) {
+      const error = new Error("All Fields Required");
+      error.statusCode = 400;
+      return next(error);
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (!existingUser) {
+      const error = new Error("User Not Found");
+      error.statusCode = 404;
+      return next(error);
+    }
+
+    existingUser.fullName = fullName;
+    existingUser.phone = phone;
+
+    await existingUser.save();
+
+    res
+      .status(200)
+      .json({ message: "User Updated Successfully", data: existingUser });
+  } catch (error) {
+    console.log(error);
+    next(error);
   }
 };
